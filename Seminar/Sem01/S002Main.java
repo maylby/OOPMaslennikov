fdac6c997ed902d48ca8a1f0339099ebccfd047a
Vit <mayllby@gmail.com>
2024-02-27 19:11:57 +0300

    Добавил код

--- Seminar/Sem01/S002Main.java
+++ Seminar/Sem01/S002Main.java
@@ -11,5 +11,25 @@ https://gb.ru/lessons/414496
 package OOP.Seminar.Sem01;
 
 public class S002Main {
-    
+    static S002Cat cat = new S002Cat();
+    public static void main(String[] args) {
+        cat.setName("boris");
+        cat.setAge(5);
+        System.out.println(cat);
+        badMethod();
+        RussianGreyCat russianGreyCat = new RussianGreyCat();
+        russianGreyCat.voice();
+        Animal.sayHello();
+
+    }
+
+
+
+    public static void badMethod(){
+        cat.setName("murzik");
+
+
+        System.out.println(cat);
+
+    }
 }
