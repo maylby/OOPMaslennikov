8264832775a41ba6acf3b09842c7077f7f76e899
Vit <mayllby@gmail.com>
2024-02-27 21:57:17 +0300

    Добавил пояснения

--- Seminar/Sem01/S002Animal.java
+++ Seminar/Sem01/S002Animal.java
@@ -9,10 +9,18 @@ https://gb.ru/lessons/414496
  */
 package OOP.Seminar.Sem01;
 
+/*
+ * Абстрактный класс
+ * позволяет писать абстрактные методы
+ */
 public abstract class S002Animal {
     String test;
-    public static void sayHello(){
+    /*
+     * "static" привязывает метод не к экземпляру класса, а к самому классу
+     */
+    public static void sayHello() {
         System.out.println("sayHello");
     }
-    public abstract void voice();
+    public abstract void voice(); // Не указана реализация, т.к.
+                                  // метод абстрактный
 }
