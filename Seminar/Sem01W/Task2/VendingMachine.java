14c3a55993ba66d06ee9e4c400c884aac1198fbb
Vit <mayllby@gmail.com>
2024-03-01 16:21:45 +0300

    Добавил код

--- Seminar/Sem01W/Task2/VendingMachine.java
+++ Seminar/Sem01W/Task2/VendingMachine.java
@@ -5,23 +5,18 @@ https://gb.ru/lessons/414496
 */
 package OOP.Seminar.Sem01W.Task2;
 
-import java.util.ArrayList;
-
-public class VendingMachine {
-
-        private ArrayList<Product> products;
+public interface VendingMachine {
     
-    public void initProducts(ArrayList<Product> products){
-        this.products = products;
-    }
     
-    public Product getProduct(String prodName){
-        for (Product p : products) {
-            if (p.getName().equals(prodName)){
-                return p;
-            }
-        }
-        return null;
-    }
+    Product getProduct(String prodName); // "public" - удалил перед "Product"
+                                         // вместо скобок "{}", точка с запятой
+    // {
+    //     for (Product p : products) {
+    //         if (p.getName().equals(prodName)){
+    //             return p;
+    //         }
+    //     }
+    //     return null;
+    // }
     
 }
