7bb7a74c38c2a0d98d7e8a91dcec63e1ad433c92
Vit <mayllby@gmail.com>
2024-03-01 18:14:54 +0300

    Добавил abstract class

--- Seminar/Sem01W/Task2/All.java
+++ Seminar/Sem01W/Task2/All.java
@@ -40,7 +40,6 @@ class Main {
 
 interface VendingMachine {
     
-    
     Object getProduct(String prodName); // "public" - удалил перед "Product"
                                          // вместо скобок "{}", точка с запятой
         // {
@@ -51,5 +50,96 @@ interface VendingMachine {
         //     }
         //     return null;
         // }
+}
+
+
+
+abstract class Product {
+
+    private String name;
+    private double cost;
+
+    public Product(String name, double cost) {
+        this.name = name;
+        this.cost = cost;
+    }
+
+    // public Product() {
+
+    // }
+
+    public String getName() {
+        return name;
+    }
+        
+    public void setName(String name) {
+        this.name = name;
+    }
+        
+    public double getCost() {
+        return cost;
+    }
+        
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    /*
+     * Переопределение метода
+     * для выдачи сведений о продукте
+     * в понятной потребителю форме
+     */
+    @Override
+    public String toString() {
+        return "Product{" +
+        "name: '" + name + '\'' +
+        ", cost: " + cost +
+        '}';
+    }
     
+}
+
+/*
+ * BottleWater Класс "наследник"
+ */
+class BottleWater extends Product {
+    String name;
+    double cost;
+
+    public BottleWater(String name, double cost) {
+        super(name, cost);
+    }
+}
+
+/*
+ * Burger - Класс "наследник"
+ * (неиспользован)
+ */
+class Burger {
+    public Burger() {
+        super();
+    }
+}
+
+
+class VendMach implements VendingMachine {
+    
+    /*
+     * Автомат по подаже бутилированной воды
+     */
+    private ArrayList<Product> bottleWaters;
+
+    public void initProducts(ArrayList<Product> battleOfWaters) {
+        this.bottleWaters = battleOfWaters;
+    }
+
+    @Override
+    public Product getProduct(String prodName) {
+        for (Product p : bottleWaters) {
+            if (p.getName().equals(prodName)) {
+            return p;
+            }
+        }
+        return null;
+    }
 }
