9341c650e7c116d8791c9a979769de7843f72e27
Vit <mayllby@gmail.com>
2024-03-01 10:57:54 +0300

    Добавил условие

--- Seminar/Sem01W/S1Product.java
+++ Seminar/Sem01W/S1Product.java
@@ -11,6 +11,14 @@ https://gb.ru/lessons/414496
  */
 
 package OOP.Seminar.Sem01W;
+/*
+* Реализуйте класс Товар, содержащий данные о товаре, и ТорговыйАвтомат,
+* содержащий в себе методы initProducts (List <Product>)
+* сохраняющий в себе список исходных продуктов и getProduct(String name) 
+*/
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class S1Product {
     
