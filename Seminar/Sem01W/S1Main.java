bec3b198c190d15b1a62f2eda6a93ab4df63c767
Vit <mayllby@gmail.com>
2024-03-01 14:52:29 +0300

    Добавил код

---
+++ Seminar/Sem01W/S1Main.java
@@ -0,0 +1,18 @@
+package OOP.Seminar.Sem01W;
+
+import java.util.ArrayList;
+
+public class S1Main {
+
+    public static void main(String[] args) {
+        ArrayList<S1Product> prods = new ArrayList<>();
+        prods.add(new S1Product("Хлеб", 40.0));
+        prods.add(new S1Product("Молоко", 80.0));
+        prods.add(new S1Product("Колбаса", 120.0));
+        prods.add(new S1Product("Сыр", 150.0));
+        
+        VendingMachine vendingMachine = new VendingMachine();
+        vendingMachine.initProducts(prods);
+        System.out.println(vendingMachine.getProduct("Сыр"));
+    }
+}
