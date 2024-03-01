e35e6f832a16d76d5dbb567290a5e7e7a1d765b7
Vit <mayllby@gmail.com>
2024-03-01 18:10:22 +0300

    Добавил 'Main'

--- Seminar/Sem01W/Task2/All.java
+++ Seminar/Sem01W/Task2/All.java
@@ -10,6 +10,9 @@ https://gb.ru/lessons/414496
 
 */
 package OOP.Seminar.Sem01W.Task2;
+
+import java.util.ArrayList;
+
 /*
  * Сделать класс Товар абстрактным, 
  * создать нескольких наследников (к примеру: БутылкаВоды), 
@@ -21,4 +24,16 @@ package OOP.Seminar.Sem01W.Task2;
  */
 public class All {
     
+}
+class Main {
+    public static void main(String[] args) {
+        ArrayList<Product> prods = new ArrayList<>();
+        prods.add(new BottleWater("Вода", 40.0));
+        prods.add(new BottleWater("Минералка", 60.0));
+        prods.add(new BottleWater("Кола", 80.0));
+        
+        VendMach vendingMachine = new VendMach();
+        vendingMachine.initProducts(prods);
+        System.out.println(vendingMachine.getProduct("Минералка"));
+    }
 }
