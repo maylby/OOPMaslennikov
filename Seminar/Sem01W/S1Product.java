1344d086b8b32b1486b05f8d0739799f432b48ca
Vit <mayllby@gmail.com>
2024-03-01 11:08:48 +0300

    Добавил код

--- Seminar/Sem01W/S1Product.java
+++ Seminar/Sem01W/S1Product.java
@@ -21,5 +21,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class S1Product {
-    
+
+    private String name;
+    private double cost;
+
+    public String getName() {
+        return name;
+    }
+        
+    public void setName(String name) {
+        this.name = name;
+    }
+        
+    public double getCost() {
+        return cost;
+    }
+        
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+        
+    public S1Product(String name, double cost) {
+        this.name = name;
+        this.cost = cost;
+    }
 }
+
+
