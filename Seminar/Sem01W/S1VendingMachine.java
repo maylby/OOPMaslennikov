5bd78afa65df1beeec1f4e43085a28d236be406e
Vit <mayllby@gmail.com>
2024-03-01 15:00:32 +0300

    Добавил код

---
+++ Seminar/Sem01W/S1VendingMachine.java
@@ -0,0 +1,34 @@
+/*
+Урок 1. Принципы ООП: Инкапсуляция, наследование, полиморфизм
+https://gb.ru/lessons/414496
+
+
+00:55:00
+
+Задача 1
+(Вариант-2 c разбиакой кода по отдельным файлам)
+
+Решение
+(Сергей Пономарев)
+
+ */
+package OOP.Seminar.Sem01W;
+
+import java.util.ArrayList;
+
+public class S1VendingMachine {
+        private ArrayList<S1Product> products;
+    
+    public void initProducts(ArrayList<S1Product> products){
+        this.products = products;
+    }
+    
+    public S1Product getProduct(String prodName){
+        for (S1Product p : products) {
+            if (p.getName().equals(prodName)){
+                return p;
+            }
+        }
+        return null;
+    }
+}
