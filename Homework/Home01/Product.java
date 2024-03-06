20b4633f880952b156a62fd5d9db3b6943ac78da
Vit <mayllby@gmail.com>
2024-03-06 19:19:39 +0300

    Добавил правки

--- Homework/Home01/Product.java
+++ Homework/Home01/Product.java
@@ -60,8 +60,8 @@ public class Product {
      */
     @Override
     public String toString() {
-        return "Product{" +
-        "name = '" + name + '\'' +
+        return "Product {" +
+        "name: '" + name + '\'' +
         ", volume: " + volume + 
         ", temperature: " + termo +
         ", cost: " + cost +
