344b886803897a6534980e0a5fc467c598731b66
Vit <mayllby@gmail.com>
2024-03-06 17:58:00 +0300

    Добавил код 'Product'

---
+++ Homework/Home01/Product.java
@@ -0,0 +1,52 @@
+package OOP.Homework.Home01;
+
+public class Product {
+
+    private String name;
+    private int volume;
+    private int termo;
+
+    public String getName() {
+        return name;
+    }
+        
+    public void setName(String name) {
+        this.name = name;
+    }
+        
+    public double getVolume() {
+        return volume;
+    }
+        
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public int getTermo() {
+        return termo;
+    }
+        
+    public void setTermo(int termo) {
+        this.termo = termo;
+    }
+        
+    public Product(String name, int volume, int termo) {
+        this.name = name;
+        this.volume = volume;
+        this.termo = termo;
+    }
+    
+    /*
+     * Переопределение метода
+     * для выдачи сведений о продукте
+     * в понятной потребителю форме
+     */
+    @Override
+    public String toString() {
+        return "Product{" +
+        "name = '" + name + '\'' +
+        ", volume: " + volume + 
+        ", temperature: " + termo +
+        '}';
+    }
+}
