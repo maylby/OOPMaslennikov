0a6e17d907771516ee3c50992b74c733b09e928c
Vit <mayllby@gmail.com>
2024-03-06 18:18:20 +0300

    Добавил код 'VendingMachine'

--- Homework/Home01/VendingMachine.java
+++ Homework/Home01/VendingMachine.java
@@ -7,6 +7,21 @@ https://gb.ru/lessons/414496/homework
  */
 package OOP.Homework.Home01;
 
+import java.util.ArrayList;
+
 public class VendingMachine {
+            private ArrayList<Product> products;
     
+    public void initProducts(ArrayList<Product> products){
+        this.products = products;
+    }
+    
+    public Product getProduct(String prodName){
+        for (Product p : products) {
+            if (p.getName().equals(prodName)){
+                return p;
+            }
+        }
+        return null;
+    }
 }
