c691e19767ed114f27ec85d057bb1245016572e3
Vit <mayllby@gmail.com>
2024-03-07 23:11:26 +0300

    Добавил условие ДЗ

--- Seminar/Sem02/Ex02/Market.java
+++ Seminar/Sem02/Ex02/Market.java
@@ -48,3 +48,21 @@ public class Market implements MarketBehaviour, QueueBehaviour {
 		// void update = true;
 	}
 }
+
+/*
+Домашнее задание
+
+Реализовать класс Market и все методы, которые он обязан реализовывать. 
+Методы из интерфейса QueueBehaviour, имитируют работу очереди, 
+MarketBehaviour – помещает и удаляет человека из очереди, 
+метод update – обновляет состояние магазина (принимает и отдает заказы)
+
+
+<<class>>
+Market
+
+void: acceptToMarket 
+void: acceptToMarket 
+void: acceptToMarket
+
+ */
