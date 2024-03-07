50e9495ee60b5f41a7afded7fc05f9567372a341
Vit <mayllby@gmail.com>
2024-03-07 17:36:20 +0300

    Добавил вызов методов

--- Seminar/Sem02/Ex01/Main.java
+++ Seminar/Sem02/Ex01/Main.java
@@ -8,5 +8,10 @@ https://gb.ru/lessons/414497
 package OOP.Seminar.Sem02.Ex01;
 
 public class Main {
-    
+    public static void main(String[] args) {
+		Cat cat = new Cat();
+		Owl owl = new Owl();
+		cat.voice();
+		owl.voice();
+    }
 }
