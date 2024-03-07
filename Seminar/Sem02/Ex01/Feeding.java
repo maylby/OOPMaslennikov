f59e33cbc4d9493f88515091436095770c72fc80
Vit <mayllby@gmail.com>
2024-03-07 18:40:49 +0300

    Добавил правки

--- Seminar/Sem02/Ex01/Feeding.java
+++ Seminar/Sem02/Ex01/Feeding.java
@@ -4,6 +4,7 @@ package OOP.Seminar.Sem02.Ex01;
  * Интерфейс "Feeding" (кормление) 
  * создан для реализации различных методов, т.к.
  * их реализация неоднотипна, в каждом методе своя
+ * ("interface" - обновлённая версия "abstract class")
  */
 public interface Feeding {
     void feed();
