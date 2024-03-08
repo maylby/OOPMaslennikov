9c5b1880fdb3d664562369428564c8eaac2d03e7
Vit <mayllby@gmail.com>
2024-03-08 06:08:07 +0300

    Добавил код

--- Lesson/Les03/Foo.java
+++ Lesson/Les03/Foo.java
@@ -29,6 +29,29 @@ https://gb.ru/lessons/414491
  */
 package OOP.Lesson.Les03;
 
-public class Ex000 {
-    
+public abstract class Foo {
+	public abstract void m1();
+	public abstract void m2();
+
+}
+
+
+class Program {
+	public static void main(String[] args) {
+		Foo foo1 = new Foo() {
+			@Override
+			public void m1() {
+				System.out.print(">>>m1");
+			}
+
+			@Override
+			public void m2() {
+				System.out.print("m2");
+			}	
+		}; // зачем точка с запятой после фигурной скобки?
+
+		// foo1.m1(); // переменная foo1 вызывает метод m1 с заданным поведением "m1"
+		foo1.m1(); // замена переменной на foo2 и вызов, того же метода (m1)
+			   // после смены его поведения с (m1) на (>>>m1)
+	}
 }
