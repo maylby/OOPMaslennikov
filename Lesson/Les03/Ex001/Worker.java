f9ea97f29d5c86914cd2c757c37366a558dd6705
Vit <mayllby@gmail.com>
2024-03-09 11:59:59 +0300

    Добавил код

--- Lesson/Les03/Ex001/Worker.java
+++ Lesson/Les03/Ex001/Worker.java
@@ -7,5 +7,22 @@ https://gb.ru/lessons/414491
 package OOP.Lesson.Les03.Ex001;
 
 public class Worker {
-    
+    public String firstName;
+    public String lastName;
+    public int age;
+    public int salary;
+
+    public Worker(String firstName,
+            String lastName,
+            int age,
+            int salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public String fullName() {
+        return String.format("%s %s", firstName, lastName);
+    }
 }
