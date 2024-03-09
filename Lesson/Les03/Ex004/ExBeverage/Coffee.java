131f047a15e8374292c5fc8454980acfbf74493f
Vit <mayllby@gmail.com>
2024-03-09 20:37:52 +0300

    Добавил код

---
+++ Lesson/Les03/Ex004/ExBeverage/Coffee.java
@@ -0,0 +1,36 @@
+/*
+Объектно-ориентированное программирование (лекции)
+Урок 3. Некоторые стандартные интерфейсы Java и примеры их использования
+https://gb.ru/lessons/414491
+
+ */
+
+package OOP.Lesson.Les03.Ex004.ExBeverage;
+
+import java.util.Iterator;
+
+public class Coffee extends Beverage {
+
+    @Override
+    public Iterator<Ingredient> iterator() {
+
+        Iterator<Ingredient> it = new Iterator<Ingredient>() {
+
+            private int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index < components.size();
+            }
+
+            @Override
+            public Ingredient next() {
+                return components.get(index++);
+            }
+
+        };
+        return it;
+
+    }
+
+}
