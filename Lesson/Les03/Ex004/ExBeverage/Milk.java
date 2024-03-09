9451f34f08e9f587b7c9d850de27b8da3fca721a
Vit <mayllby@gmail.com>
2024-03-09 20:42:00 +0300

    Добавил код

---
+++ Lesson/Les03/Ex004/ExBeverage/Milk.java
@@ -0,0 +1,15 @@
+/*
+Объектно-ориентированное программирование (лекции)
+Урок 3. Некоторые стандартные интерфейсы Java и примеры их использования
+https://gb.ru/lessons/414491
+
+ */
+
+package OOP.Lesson.Les03.Ex004.ExBeverage;
+
+public class Milk extends Ingredient {
+
+    public Milk(String brand) {
+        super(brand);
+    }    
+}
