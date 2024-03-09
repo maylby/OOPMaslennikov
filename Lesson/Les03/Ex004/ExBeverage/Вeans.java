3c168e94f7f7945b156cec46126131aef7924d99
Vit <mayllby@gmail.com>
2024-03-09 20:46:14 +0300

    Добавил код

---
+++ Lesson/Les03/Ex004/ExBeverage/Вeans.java
@@ -0,0 +1,14 @@
+/*
+Объектно-ориентированное программирование (лекции)
+Урок 3. Некоторые стандартные интерфейсы Java и примеры их использования
+https://gb.ru/lessons/414491
+
+ */
+package OOP.Lesson.Les03.Ex004.ExBeverage;
+
+public class Вeans extends Ingredient {
+
+    public Вeans(String brand) {
+        super(brand);
+    }    
+}
