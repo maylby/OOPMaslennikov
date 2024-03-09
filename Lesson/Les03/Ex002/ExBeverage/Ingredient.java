a099fd2f779ffea456de6c24c33341f5b7342902
Vit <mayllby@gmail.com>
2024-03-09 19:42:30 +0300

    Добавил код

--- Lesson/Les03/Ex002/ExBeverage/Ingredient.java
+++ Lesson/Les03/Ex002/ExBeverage/Ingredient.java
@@ -7,6 +7,15 @@ https://gb.ru/lessons/414491
 
 package OOP.Lesson.Les03.Ex002.ExBeverage;
 
-public class Ingredient {
-    
+public abstract class Ingredient {
+    public String brand;
+
+    public Ingredient(String brand) {
+        this.brand = brand;
+    }
+
+    @Override
+    public String toString() {
+        return brand;
+    }
 }
