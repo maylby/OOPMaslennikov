1f9bb08dcb575360ab26d09ceee865ec373878b2
Vit <mayllby@gmail.com>
2024-03-09 23:20:04 +0300

    Добавил заголовок

--- Lesson/Les03/Ex006/Worker1.java
+++ Lesson/Les03/Ex006/Worker1.java
@@ -1,3 +1,9 @@
+/*
+Объектно-ориентированное программирование (лекции)
+Урок 3. Некоторые стандартные интерфейсы Java и примеры их использования
+https://gb.ru/lessons/414491
+
+ */
 package OOP.Lesson.Les03.Ex006;
 
 public class Worker1 implements Comparable<Worker1> {
