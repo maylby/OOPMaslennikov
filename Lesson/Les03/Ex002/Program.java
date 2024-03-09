7a99783c63b737a9d8cfabfc274d0c4a81fde06c
Vit <mayllby@gmail.com>
2024-03-09 12:46:46 +0300

    Добавил код

--- Lesson/Les03/Ex002/Program.java
+++ Lesson/Les03/Ex002/Program.java
@@ -12,7 +12,8 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import Lesson_09.Ex002.ExBeverage.*;
+import OOP.Lesson.Les03.Ex002.ExBeverage.*;
+
 
 public class Program {
     public static void main(String[] args) {
