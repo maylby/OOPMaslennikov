3ae149c267c99b5b95c143ffb4addd9ac6e91607
Vit <mayllby@gmail.com>
2024-03-10 20:02:02 +0300

    Добавил 'QueueBehaviour'

---
+++ Homework/Home02/Solution/QueueBehaviour.java
@@ -0,0 +1,23 @@
+/*
+Объектно-ориентированное программирование (семинары)
+Урок 2. Принципы ООП Абстракция и интерфейсы. 
+
+https://gb.ru/lessons/414497
+
+ */
+
+package OOP.Homework.Home02.Solution;
+
+/*
+ * Интерфейс QueueBehaviour 
+ * описывает логику очереди - 
+ * вход/выход из очереди,
+ * принятие/отдача заказа
+ */
+interface QueueBehaviour {
+
+	void takeInQueue(Actor actor);
+	void takeOrders();
+	void giveOrders();
+	void releaseFromQueue();
+}
