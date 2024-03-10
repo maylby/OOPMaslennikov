f56bb905d292764922c71eb1972680539acd6640
Vit <mayllby@gmail.com>
2024-03-10 20:41:37 +0300

    Добавил условие ДЗ

--- Homework/Home02/Solution/Market.java
+++ Homework/Home02/Solution/Market.java
@@ -4,6 +4,21 @@
 
 https://gb.ru/lessons/414497/homework
 
+Домашнее задание (ДЗ-2)
+
+Реализовать класс Market и все методы, которые он обязан реализовывать. 
+Методы из интерфейса QueueBehaviour, имитируют работу очереди, 
+MarketBehaviour – помещает и удаляет человека из очереди, 
+метод update – обновляет состояние магазина (принимает и отдает заказы)
+
+
+<<class>>
+Market
+
+void: acceptToMarket 
+void: acceptToMarket 
+void: acceptToMarket
+
 
 Реализация (класс "Market")
 
