6b65a137dab76a6b3f8c86487c766cd1aad81798
Vit <mayllby@gmail.com>
2024-03-10 15:28:42 +0300

    Добавил условие ДЗ

--- Seminar/Sem02/Ex03/ActAll.java
+++ Seminar/Sem02/Ex03/ActAll.java
@@ -174,3 +174,26 @@ class Market implements MarketBehaviour, QueueBehaviour {
 	}
 }
 
+/*
+Домашнее задание 2
+
+ * Рекомендации по выполнению ДЗ
+ *
+ * "QueueBehaviour" - это очередь
+ * прописать поля ArrayList (реализация класса "Market" (?))
+ * в "Market" взаимодействовать с полями
+
+Реализовать класс Market и все методы, которые он обязан реализовывать. 
+Методы из интерфейса QueueBehaviour, имитируют работу очереди, 
+MarketBehaviour – помещает и удаляет человека из очереди, 
+метод update – обновляет состояние магазина (принимает и отдает заказы)
+
+
+<<class>>
+Market
+
+void: acceptToMarket 
+void: acceptToMarket 
+void: acceptToMarket
+
+ */
