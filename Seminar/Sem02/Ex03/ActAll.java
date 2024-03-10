8ea45acdbf9d999f92f13e7d819484c96b5661cf
Vit <mayllby@gmail.com>
2024-03-10 15:28:10 +0300

    Добавил код

--- Seminar/Sem02/Ex03/ActAll.java
+++ Seminar/Sem02/Ex03/ActAll.java
@@ -132,3 +132,45 @@ class Human extends ActAll {
 		return isTakeOrder;
 	} 
 }
+
+
+
+class Market implements MarketBehaviour, QueueBehaviour {
+
+	
+	@Override
+	public void takeInQueue(ActAll actor) {
+		// return null;
+	}
+
+	@Override
+	public void takeOrders() {
+		// return null;
+	}
+
+	@Override
+	public void giveOrders() {
+		// giveOrders = true;
+	}
+
+	@Override
+	public void releaseFromQueue() {
+		// releaseFromQueue = true;
+	}
+
+	@Override
+	public void acceptToMarket(ActAll actor) {
+		// acceptToMarket(Actor actor) = true;
+	}
+
+	@Override
+	public void releaseFromMarket(List<Actor> actors) {
+		// releaseFromMarket(List<Actor> actors) = true;
+	}
+
+	@Override
+	public void update() {
+		// void update = true;
+	}
+}
+
