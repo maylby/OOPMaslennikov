1bceaab4bc67ce0150dc9698ba4694e09a247765
Vit <mayllby@gmail.com>
2024-03-12 23:26:44 +0300

    Добавил код

--- Homework/Home03/StreamComparator.java
+++ Homework/Home03/StreamComparator.java
@@ -7,6 +7,13 @@ https://gb.ru/lessons/414498/homework
 
 package OOP.Homework.Home03;
 
-public class StreamComparator {
-    
-}
+import java.util.Comparator;
+
+public class StreamComparator implements Comparator<Student> {
+     @Override
+     public int compare(Student g1, Student g2) {
+         String s1 = g1.getSpecialization();
+         String s2 = g2.getSpecialization();
+         return s1.compareTo(s2);
+     }
+ }
