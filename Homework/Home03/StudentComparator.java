71845de29e2a0c4637b41097944ce5e3c9493b07
Vit <mayllby@gmail.com>
2024-03-12 21:03:19 +0300

    Добавил код

--- Homework/Home03/StudentComparator.java
+++ Homework/Home03/StudentComparator.java
@@ -5,3 +5,15 @@ https://gb.ru/lessons/414498/homework
 
  */
 
+ package OOP.Homework.Home03;
+
+ import java.util.Comparator;
+ 
+ public class StudentComparator implements Comparator<Student> {
+     @Override
+     public int compare(Student t1, Student t2) {
+         String s1 = t1.getName() + t1.getAge();
+         String s2 = t2.getName() + t2.getAge();
+         return s1.compareTo(s2);
+     }
+ }
