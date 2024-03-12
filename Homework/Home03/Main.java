39f37a3f1150fdfc458bd9113ac1b05a14e4233c
Vit <mayllby@gmail.com>
2024-03-12 22:05:59 +0300

    Добавил правки

--- Homework/Home03/Main.java
+++ Homework/Home03/Main.java
@@ -32,12 +32,28 @@ https://gb.ru/lessons/414498/homework
   */
  public class Main {
      public static void main(String[] args) {
-         Student s1 = new Student("Q", 20, "Chemistry");
-         Student s5 = new Student("Q", 26, "Chemistry");
-         Student s2 = new Student("A", 22, "Chemistry");
-         Student s3 = new Student("Z", 24, "Chemistry");
-         Student s4 = new Student("E", 21, "Chemistry");
-         ArrayList<Student> students = new ArrayList<>(List.of(s1, s2, s3, s4, s5));
+         Student s01 = new Student("Q", 20, "Chemistry", 5);
+         Student s02 = new Student("Q", 26, "Chemistry", 4);
+         Student s03 = new Student("A", 22, "Chemistry", 5);
+         Student s04 = new Student("A", 22, "Chemistry", 4);
+         Student s05 = new Student("Z", 24, "Chemistry", 3);
+         Student s06 = new Student("E", 21, "Chemistry", 4);
+        //  ArrayList<Student> students = new ArrayList<>(List.of(s1, s2, s3, s4, s5, s6));
+
+         Student s07 = new Student("C", 20, "Physics", 5);
+         Student s08 = new Student("C", 22, "Physics", 4);
+         Student s09 = new Student("A", 22, "Physics", 5);
+         Student s10 = new Student("C", 22, "Physics", 5);
+         Student s11 = new Student("Z", 24, "Physics", 3);
+
+         Student s12 = new Student("B", 26, "History", 5);
+         Student s13 = new Student("B", 22, "History", 5);
+         Student s14 = new Student("A", 22, "History", 5);
+         Student s15 = new Student("F", 24, "History", 3);
+
+         ArrayList<Student> students = new ArrayList<>(List.of(s01, s02, s03, s04, s05, s06, 
+                                                                s07, s08, s09, s10, s11,
+                                                                s12, s13, s14, s15));
  
          Group group = new Group(students);
  
