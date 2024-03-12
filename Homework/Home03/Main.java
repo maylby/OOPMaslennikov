5940ef02679c345bde674df73321479e649c4bbe
Vit <mayllby@gmail.com>
2024-03-12 20:57:58 +0300

    Добавил ссылку

--- Homework/Home03/Main.java
+++ Homework/Home03/Main.java
@@ -1,9 +1,7 @@
 /*
 Объектно-ориентированное программирование (семинары)
 Урок 3. Некоторые стандартные интерфейсы Java и примеры их использования
-https://gb.ru/lessons/414498
-
-https://github.com/ponomarevss/javaStudy/blob/classwork3/oop/lesson3/Main.java
+https://gb.ru/lessons/414498/homework
 
  */
 
