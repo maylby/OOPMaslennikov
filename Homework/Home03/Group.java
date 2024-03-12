54c942485555922a042b8ea48603b2ab29a92d5c
Vit <mayllby@gmail.com>
2024-03-12 21:00:45 +0300

    Добавил заголовок

--- Homework/Home03/Group.java
+++ Homework/Home03/Group.java
@@ -1,25 +1,6 @@
-package OOP.Seminar.Sem03.Lesson3;
+/*
+Объектно-ориентированное программирование (семинары)
+Урок 3. Некоторые стандартные интерфейсы Java и примеры их использования
+https://gb.ru/lessons/414498/homework
 
-import java.util.ArrayList;
-import java.util.Iterator;
-
-public class Group implements Iterable<Student>{
-    private ArrayList<Student> students;
-
-    public Group(ArrayList<Student> students) {
-        this.students = students;
-    }
-
-    public ArrayList<Student> getStudents() {
-        return students;
-    }
-
-    public void setStudents(ArrayList<Student> students) {
-        this.students = students;
-    }
-
-    @Override
-    public Iterator<Student> iterator() {
-        return new GroupIterator(students);
-    }
-}
+ */
