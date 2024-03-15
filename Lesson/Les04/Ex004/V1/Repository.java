3a05f820240230276b754c5c4e6061bade641f3f
Vit <mayllby@gmail.com>
2024-03-15 19:35:46 +0300

    Добавил код

--- Lesson/Les04/Ex004/V1/Repository.java
+++ Lesson/Les04/Ex004/V1/Repository.java
@@ -8,6 +8,40 @@ https://gb.ru/lessons/414492
  */
 package OOP.Lesson.Les04.Ex004.V1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Repository {
+    List<Content> ds;
+
+    private String name;
+
+    /*
+    * Конструктор
+    */
+    public Repository(String name) {
+        this.ds = new ArrayList<>();
+        this.name = name;
+        System.out.printf("\n  >>  %s created\n",this.name); // консоль для подтверждения
+							                                        // факта использования репозитория,
+							                                        // но этого здесь быть не должно 
+    }
     
+    /*
+    * Метод лобавления контента
+    */ 
+    public void add(Content content) {
+        ds.add(content);
+    }
+
+    public int count() {
+        return ds.size();
+    }
+
+    public Content get(Integer index) {
+        return ds.get(index);
+    }
+    
+    // В качестве тренировки реализуйте возможность работы с foreach
+
 }
