16af92d1f53d3bc0499d3212654f096d8566628c
Vit <mayllby@gmail.com>
2024-03-15 19:22:14 +0300

    Добавил код

---
+++ Lesson/Les04/Ex004/V1/VideoContent.java
@@ -0,0 +1,17 @@
+/*
+Объектно-ориентированное программирование (лекции)
+Урок 4. ООП: Обобщения
+https://gb.ru/lessons/414492
+
+ */
+package OOP.Lesson.Les04.Ex004.V1;
+
+/*
+ * Класс "VideoContent", наследующий 
+ * абстрактный класс "Content"
+ */
+public class VideoContent extends Content {
+    public VideoContent(String name) {
+        super(name);
+    }
+}
