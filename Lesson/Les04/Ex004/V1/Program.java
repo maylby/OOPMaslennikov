c50dbb0db499d9aa6e5c362cecc6eae6723dfb5f
Vit <mayllby@gmail.com>
2024-03-15 19:39:31 +0300

    Добавил заголовок

---
+++ Lesson/Les04/Ex004/V1/Program.java
@@ -0,0 +1,11 @@
+/*
+Объектно-ориентированное программирование (лекции)
+Урок 4. ООП: Обобщения
+https://gb.ru/lessons/414492
+
+ */
+package OOP.Lesson.Les04.Ex004.V1;
+
+public class Program {
+    
+}
