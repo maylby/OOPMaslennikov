da147dd80b417ebd8fde02b805c27c3412614b79
Vit <mayllby@gmail.com>
2024-03-15 18:13:17 +0300

    Добавил заголовок

---
+++ Lesson/Les04/Ex003/ParameterizedWorker.java
@@ -0,0 +1,11 @@
+/*
+Объектно-ориентированное программирование (лекции)
+Урок 4. ООП: Обобщения
+https://gb.ru/lessons/414492
+
+ */
+package OOP.Lesson.Les04.Ex003;
+
+public class ParameterizedWorker {
+    
+}
