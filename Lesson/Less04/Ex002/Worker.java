4c0d05494d3419dc1e4bef01b5783b6e8b31182e
Vit <mayllby@gmail.com>
2024-03-15 09:28:22 +0300

    Добавил код

--- Lesson/Less04/Ex002/Worker.java
+++ Lesson/Less04/Ex002/Worker.java
@@ -8,3 +8,31 @@ https://gb.ru/lessons/414492
 "Воркер" (рабочий)
  */
 
+ package OOP.Lesson.Less04.Ex002;
+
+ public class Worker {
+     private int id;
+ 
+     public String firstName;
+     public String lastName;
+     public int age;
+     public int salary;
+ 
+     public Worker(int id, String firstName,
+             String lastName,
+             int age,
+             int salary) {
+ 
+         this.id = id;
+         this.firstName = firstName;
+         this.lastName = lastName;
+         this.age = age;
+         this.salary = salary;
+         
+     }
+ 
+     public String fullName() {
+         return String.format("%s %s", firstName, lastName);
+     }
+ }
+ 
