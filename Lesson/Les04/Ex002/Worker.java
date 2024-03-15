04167154e379d8a594ca9b1e2cad9f8d71d5cb0b
Vit <mayllby@gmail.com>
2024-03-15 10:33:07 +0300

    Добавил правки

--- Lesson/Les04/Ex002/Worker.java
+++ Lesson/Les04/Ex002/Worker.java
@@ -8,31 +8,31 @@ https://gb.ru/lessons/414492
 "Воркер" (рабочий)
  */
 
- package OOP.Lesson.Les04.Ex002;
+package OOP.Lesson.Les04.Ex002;
 
- public class Worker {
-     private int id;
+public class Worker {
+    public /*private*/ int id; // "private" изменил на "public",
+                               // с "private" система подчёркивала "id" жёлтым
+    public String firstName;
+    public String lastName;
+    public int age;
+    public int salary;
  
-     public String firstName;
-     public String lastName;
-     public int age;
-     public int salary;
+    public Worker(int id, String firstName,
+            String lastName,
+            int age,
+            int salary) {
  
-     public Worker(int id, String firstName,
-             String lastName,
-             int age,
-             int salary) {
- 
-         this.id = id;
-         this.firstName = firstName;
-         this.lastName = lastName;
-         this.age = age;
-         this.salary = salary;
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.salary = salary;
          
-     }
+    }
  
-     public String fullName() {
-         return String.format("%s %s", firstName, lastName);
-     }
- }
+    public String fullName() {
+        return String.format("%s %s", firstName, lastName);
+    }
+}
  
