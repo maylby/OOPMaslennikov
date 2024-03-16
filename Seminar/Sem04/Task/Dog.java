0ced9efeff8c5894d343934503f3bfd0c8496131
Vit <mayllby@gmail.com>
2024-03-16 20:16:38 +0300

    Добавил код

---
+++ Seminar/Sem04/Task/Dog.java
@@ -0,0 +1,22 @@
+package OOP.Seminar.Sem04.Task;
+
+public class Dog {
+    private String name;
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
