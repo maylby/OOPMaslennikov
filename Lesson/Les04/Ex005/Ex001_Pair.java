b251b091a8c5a8136b8a5f719041cdcd83bd1706
Vit <mayllby@gmail.com>
2024-03-16 16:06:54 +0300

    Добавил код

--- Lesson/Les04/Ex005/Ex001_Pair.java
+++ Lesson/Les04/Ex005/Ex001_Pair.java
@@ -15,5 +15,38 @@ public class Ex001_Pair {
         System.out.println(pair.getFirst());
         System.out.println(pair.getSecond());
 
-    }    
+    }
+}
+
+class Pair<T> {
+
+    T first;
+    T second;
+
+    public Pair(T t1, T t2) {
+        this.first = t1;
+        this.second = t2;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public void setFirst(T first) {
+        this.first = first;
+    }
+
+    public T getSecond() {
+        return second;
+    }
+
+    public void setSecond(T second) {
+        this.second = second;
+    }
+
+    public void swap() {
+        T temp = first;
+        first = second;
+        second = temp;
+    }
 }
