cc86ea64cb4a7f28b25446fe13e3fe012dbe25ce
Vit <mayllby@gmail.com>
2024-03-18 11:46:09 +0300

    Добавил заголовок

---
+++ Lesson/Les05/Ex002/Core/Infrastructure/Phonebook.java
@@ -0,0 +1,15 @@
+/*
+Объектно-ориентированное программирование (лекции)
+Урок 5. ООП: От простого к практике
+https://gb.ru/lessons/414493
+
+Ядро
+
+"Core" (папка ядра)
+"Infrastructure" - Инфрасруктурный модуль (вложенная папка)
+ */
+package OOP.Lesson.Les05.Ex002.Core.Infrastructure;
+
+public class Phonebook {
+    
+}
