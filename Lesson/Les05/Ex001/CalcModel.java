b24d1d5a3d48615c1c4deb06b415630eff1a7f6e
Vit <mayllby@gmail.com>
2024-03-18 10:46:14 +0300

    Добавил код

---
+++ Lesson/Les05/Ex001/CalcModel.java
@@ -0,0 +1,16 @@
+/*
+Объектно-ориентированное программирование (лекции)
+Урок 5. ООП: От простого к практике
+https://gb.ru/lessons/414493
+*/
+
+package OOP.Lesson.Les05.Ex001;
+
+/*
+* Абстрактный класс "CalcModel", реализующий 
+* интерфейс (implements) "Model"
+*/
+public abstract class CalcModel implements Model {
+
+    int x, y;
+}
