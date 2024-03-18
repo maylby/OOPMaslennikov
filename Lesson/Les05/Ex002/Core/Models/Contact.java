ddefa0bea7685c14a0d558baed806da5260a772b
Vit <mayllby@gmail.com>
2024-03-18 11:56:24 +0300

    Добавил код

--- Lesson/Les05/Ex002/Core/Models/Contact.java
+++ Lesson/Les05/Ex002/Core/Models/Contact.java
@@ -6,6 +6,48 @@ https://gb.ru/lessons/414493
  */
 package OOP.Lesson.Les05.Ex002.Core.Models;
 
-public class Contact {
+import java.time.LocalDate;
+
+/*
+* Определение модели (Contact)
+*/
+public class Contact  implements Comparable<Contact> {
+
+    // Поля контакта
+    public LocalDate date;
+    public String firstName;
+    public String lastName;
+    public String company;
+    public String phone;
+    //public String email; // Можно использовать, как строку
+    //public Email email;  // Можно описать отдельную сущность,
+			   // модель хранения емейл-адреса 
+			   // проверка на валлидность
+			   // поэтапное введение адреса (?)
+			   // введение домена 1, 2, 3 уровней
+    public String description;
     
+    // конструктор с тремя аргументами
+    public Contact(String firstName, String lastName, String description) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.description = description;
+    }
+    
+    // добавить возможность создания объектов с бОльшим
+    // количеством параметров
+
+    @Override
+    public int compareTo(Contact o) {
+        // ???...
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Contact t = (Contact)obj;
+        return this.firstName == t.firstName
+                && this.lastName == t.lastName
+        ;
+    }
 }
