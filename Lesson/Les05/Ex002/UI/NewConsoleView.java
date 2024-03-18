6c3aa963b3c42e0347ca279a0d33026f70bf4159
Vit <mayllby@gmail.com>
2024-03-18 12:40:09 +0300

    Добавил код

---
+++ Lesson/Les05/Ex002/UI/NewConsoleView.java
@@ -0,0 +1,26 @@
+/*
+Объектно-ориентированное программирование (лекции)
+Урок 5. ООП: От простого к практике
+https://gb.ru/lessons/414493
+
+ */
+package OOP.Lesson.Les05.Ex002.UI;
+
+/*
+* Класс создания новой консоли 
+* для внесения изменений в код 
+* без изменения пержних записей (ветвление)
+*/
+public class NewConsoleView extends ConsoleView {
+    
+    public NewConsoleView() {
+        super();
+    }
+
+    @Override
+    public void setDescription(String value) {
+        super.setDescription(value);
+        System.out.println();
+        
+    }
+}
