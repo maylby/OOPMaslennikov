b9cffa3040ebb26ed4f2ccf7a6d3ca7289f82833
Vit <mayllby@gmail.com>
2024-03-19 15:20:23 +0300

    Добавил пояснения

--- Lesson/Les05/Ex003/Mathematics/CalculateArea/Calculate.java
+++ Lesson/Les05/Ex003/Mathematics/CalculateArea/Calculate.java
@@ -8,6 +8,10 @@ package OOP.Lesson.Les05.Ex003.Mathematics.CalculateArea;
 
 import OOP.Lesson.Les05.Ex003.Mathematics.Shapes.Shape;
 
+/*
+ * Интерфейс "Calculate", содержащий 
+ * один метод (visit), который принимает фигуру
+ */
 public interface Calculate {
     double visit(Shape shape);
 }
