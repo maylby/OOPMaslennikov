d3859b3982e4300f7e19fd59cb271e20a2199911
Vit <mayllby@gmail.com>
2024-03-19 16:21:38 +0300

    Добавил код

---
+++ Lesson/Les05/Ex003/Core/Infrastructure/Generator.java
@@ -0,0 +1,11 @@
+/*
+Объектно-ориентированное программирование (лекции)
+Урок 5. ООП: От простого к практике
+https://gb.ru/lessons/414493
+
+ */
+package OOP.Lesson.Les05.Ex003.Core.Infrastructure;
+
+public class Generator {
+    
+}
