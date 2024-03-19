f453cc350187b65a6c7ee6a498cc6562ee0d180c
Vit <mayllby@gmail.com>
2024-03-19 16:04:58 +0300

    Добавил заголовок

--- Lesson/Les05/Ex003/Client/Program.java
+++ Lesson/Les05/Ex003/Client/Program.java
@@ -1,3 +1,9 @@
+/*
+Объектно-ориентированное программирование (лекции)
+Урок 5. ООП: От простого к практике
+https://gb.ru/lessons/414493
+
+ */
 package OOP.Lesson.Les05.Ex003.Client;
 
 import OOP.Lesson.Les05.Ex003.Mathematics.Exceptions.UnacceptableValueException;
