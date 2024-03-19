30f698f2df9d02b5e5f0de0bcdc4e60da8794846
Vit <mayllby@gmail.com>
2024-03-19 14:39:37 +0300

    Добавил код

---
+++ Lesson/Les05/Ex003/Mathematics/Shapes/Canvas.java
@@ -0,0 +1,19 @@
+/*
+Объектно-ориентированное программирование (лекции)
+Урок 5. ООП: От простого к практике
+https://gb.ru/lessons/414493
+
+ */
+package OOP.Lesson.Les05.Ex003.Mathematics.Shapes;
+
+/*
+ * Класс "холст" (Canvas),
+ * наследующий класс Shape
+ */
+public class Canvas extends Shape {
+    public Canvas(String name) {
+        setNane(name);
+    }
+
+    /// ???... area
+}
