f865e17ced870a7c2870864a1f3550da8ae87e0f
Vit <mayllby@gmail.com>
2024-03-19 16:34:45 +0300

    Добавил код

--- Lesson/Les05/Ex003/Core/Infrastructure/Generator.java
+++ Lesson/Les05/Ex003/Core/Infrastructure/Generator.java
@@ -6,6 +6,12 @@ https://gb.ru/lessons/414493
  */
 package OOP.Lesson.Les05.Ex003.Core.Infrastructure;
 
+import java.util.Random;
+
 public class Generator {
-    
+    static Random r = new Random();
+
+    public static int number() {
+        return r.nextInt(10, 15);
+    }
 }
