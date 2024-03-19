bca28b6e61f67096d3372d8af930200766152e29
Vit <mayllby@gmail.com>
2024-03-19 15:02:38 +0300

    Добавил правки

--- Lesson/Les05/Ex003/Mathematics/Shapes/Canvas.java
+++ Lesson/Les05/Ex003/Mathematics/Shapes/Canvas.java
@@ -7,8 +7,10 @@ https://gb.ru/lessons/414493
 package OOP.Lesson.Les05.Ex003.Mathematics.Shapes;
 
 /*
- * Класс "холст" (Canvas),
- * наследующий класс Shape
+ * Класс "холст" (Canvas) для вычисления площади фигур, т.е.
+ * некая базовая площадь, на которой можно разместить любую фигуру
+ * Нужно ли делать подобный модуль? 
+ * Ведь, всякая фигура сама по себе уже является "холстом".
  */
 public class Canvas extends Shape {
     public Canvas(String name) {
