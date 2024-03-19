1211d730e450eea1fc2fcb9ab72904d7c21dcdbe
Vit <mayllby@gmail.com>
2024-03-19 16:05:23 +0300

    Добавил код

---
+++ Lesson/Les05/Ex003/Client/ConsoleView.java
@@ -0,0 +1,25 @@
+/*
+Объектно-ориентированное программирование (лекции)
+Урок 5. ООП: От простого к практике
+https://gb.ru/lessons/414493
+
+ */
+package OOP.Lesson.Les05.Ex003.Client;
+
+import java.util.Scanner;
+
+import OOP.Lesson.Les05.Ex003.Core.Views.View;
+
+public class ConsoleView implements View {
+    Scanner in = new Scanner(System.in);
+
+    public String get() {
+        return in.next();
+    };
+
+    @Override
+    public void set(String value) {
+        System.out.println(value);
+
+    }
+}
