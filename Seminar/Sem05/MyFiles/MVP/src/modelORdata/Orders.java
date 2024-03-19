2a248e5131245c32c9f027a90b68d6c76bfe0345
Vit <mayllby@gmail.com>
2024-03-19 22:36:45 +0300

    Добавил 'Orders'

---
+++ Seminar/Sem05/MyFiles/MVP/src/modelORdata/Orders.java
@@ -0,0 +1,28 @@
+package modelORdata;
+
+import java.util.ArrayList;
+
+public class Orders {
+    private ArrayList<Shwarma> arrayList;
+
+    public Orders() {
+        arrayList = new ArrayList<>();
+    }
+    public void add(Shwarma shwarma){
+        System.out.println("добавили в orders " + shwarma);
+        arrayList.add(shwarma);
+
+    }
+
+    public void getArrayList() {
+
+        System.out.println(arrayList);
+    }
+
+    @Override
+    public String toString() {
+        return "Orders{" +
+                "arrayList=" + arrayList +
+                '}';
+    }
+}
