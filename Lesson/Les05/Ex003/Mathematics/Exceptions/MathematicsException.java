a9b5092fa836f079e4afe50e3176359b62f0c018
Vit <mayllby@gmail.com>
2024-03-19 13:56:57 +0300

    Добавил пояснения

--- Lesson/Les05/Ex003/Mathematics/Exceptions/MathematicsException.java
+++ Lesson/Les05/Ex003/Mathematics/Exceptions/MathematicsException.java
@@ -6,6 +6,10 @@ https://gb.ru/lessons/414493
  */
 package OOP.Lesson.Les05.Ex003.Mathematics.Exceptions;
 
+/*
+ * Класс для работы с математической библиотечкой, который
+ * наследует "Exception", папку, в которой размещён
+ */
 public class MathematicsException extends Exception {
     public MathematicsException(String msg) {
         super(msg);
--- Lesson/Les05/Ex003/Mathematics/Exceptions/UnacceptableValueException.java
+++ Lesson/Les05/Ex003/Mathematics/Exceptions/UnacceptableValueException.java
@@ -6,6 +6,11 @@ https://gb.ru/lessons/414493
  */
 package OOP.Lesson.Les05.Ex003.Mathematics.Exceptions;
 
+/*
+ * Выстраивание иерахии наследования 
+ * неких неваллидных данных от "MathematicsException", которое
+ * само по себе является исключением
+ */
 public class UnacceptableValueException extends MathematicsException {
     public UnacceptableValueException(String msg) {
         super(msg);
