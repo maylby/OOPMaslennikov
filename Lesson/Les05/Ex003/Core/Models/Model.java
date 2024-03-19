fe86c37f147fb38788a8716248dd02e76e4ba4a7
Vit <mayllby@gmail.com>
2024-03-19 16:25:37 +0300

    Добавил код

--- Lesson/Les05/Ex003/Core/Models/Model.java
+++ Lesson/Les05/Ex003/Core/Models/Model.java
@@ -4,7 +4,7 @@
 https://gb.ru/lessons/414493
 
  */
-package OOP.Lesson.Les05.Ex003.Core.Infrastructure;
+package OOP.Lesson.Les05.Ex003.Core.Models;
 
 import OOP.Lesson.Les05.Ex003.Mathematics.CalculateArea.Calculate;
 import OOP.Lesson.Les05.Ex003.Mathematics.Shapes.Canvas;
