22aa48c0c4dfd539ef8802b8e3ec6837c2798f36
Vit <mayllby@gmail.com>
2024-03-21 21:20:53 +0300

    Добавил введение

---
+++ Lesson/Les06/Ex000_Info.java
@@ -0,0 +1,55 @@
+
+/*
+Объектно-ориентированное программирование (лекции)
+Урок 6. SOLID
+https://gb.ru/lessons/414494
+
+План урока:
+
+- Single responsibility principle (Принцип единственной ответственности)
+- Open-closed principle (Принцип открытости/закрытости)
+- Liskov substitution principle (Принцип подстановки Лисков)
+- Interface segregation principle (Принцип разделения интерфейса)
+- Dependency inversion principle (Принцип инверсии зависимостей)
+
+
+Разговор о SOLID…
+
+1. Single responsibility principle
+   Принцип единственной ответственности
+2. Open-closed principle
+   Принцип открытости/закрытости
+3. Liskov substitution principle
+   Принцип подстановки Лисков
+4. Interface segregation principle
+   Принцип разделения интерфейса
+5. Dependency inversion principle
+   Принцип инверсии зависимостей
+
+
+Single responsibility principle
+
+Принцип гласит, что может быть только
+одна причина, приводящая к изменению класса.
+Другими словами, каждый класс должен иметь
+только одну ответственность, которая 
+должна быть инкапсулирована в этом классе.
+- тестируемость;
+- читаемость;
+- короткое и понятное описание класса;
+- простота сопровождения.
+
+
+SRP-коды в VSCode:
+
+Ex001_1SRP - время? (см. VSCode)
+Ex001_2SRP - время? (см. VSCode)
+
+Ex001_3SRP - время? (код ниже)
+
+*/
+package OOP.Lesson.Les06;
+
+public class Ex000_Info {
+    
+}
