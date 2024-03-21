55044589cd7c8c1d3953d50e0c75e01e94e02d0e
Vit <mayllby@gmail.com>
2024-03-21 21:40:14 +0300

    Добавил код

--- Lesson/Les06/Ex000_Info.java
+++ Lesson/Les06/Ex000_Info.java
@@ -337,7 +337,6 @@ class Python extends AbstractAnimal {
 }
 
 
-
 /*
 00:28:00
 
@@ -353,3 +352,65 @@ Interface segregation principle
 
 */
 
+// Interface segregation principle
+
+// Принцип разделения интерфейса 
+// Модули высокого уровня не должны зависеть от модулей нижнего уровня.
+// И те, и другие должны зависеть от абстракций.
+// Абстракции не должны зависеть от деталей. 
+// Детали должны зависеть от абстракций.
+
+/* public */ class Ex004_2ISP {
+    public static void main(String[] args) {
+
+    }
+}
+
+interface BasePhone {
+    void Call();
+    void SendSMS();
+}
+
+interface Fax {
+    void SendFax();
+
+}
+
+interface EMail {
+    void SendEMail();
+}
+
+class IPhone implements BasePhone, Fax, EMail {
+
+    @Override
+    public void Call() {
+
+    }
+
+    @Override
+    public void SendSMS() {
+    
+    }
+
+    @Override
+    public void SendEMail() {
+
+    }
+
+    @Override
+    public void SendFax() {
+        
+    }
+}
+
+class Nokia3311 implements BasePhone {
+
+    @Override
+    public void Call() {
+    }
+
+    @Override
+    public void SendSMS() {
+
+    }
+}
