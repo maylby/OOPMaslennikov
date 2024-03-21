51a0e5b8d97f040731bfe350a4e2e3802cf35dc1
Vit <mayllby@gmail.com>
2024-03-21 08:23:58 +0300

    Добавил код

---
+++ Homework/Home05/Data/Student.java
@@ -0,0 +1,28 @@
+package OOP.Homework.Home05.Data;
+/*
+* Класс "Student" реализует (наследует) 
+* абстрактный (?) класс "User", присваивая
+* "id" каждому студенту (Student)
+*/
+public class Student extends User {
+	private int id;
+
+	public Student(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [name = " + this.name + 
+					"age = " + this.age + 
+					"id = " + id + "]";
+	}}
