5d79154efbb4d2ca12584503184b4d6c2d8fffc0
Vit <mayllby@gmail.com>
2024-03-21 18:16:50 +0300

    Добавил код

---
+++ Lesson/Les06/Ex001_3SRP.java
@@ -0,0 +1,90 @@
+/*
+Объектно-ориентированное программирование (лекции)
+Урок 6. SOLID
+https://gb.ru/lessons/414494
+
+ */
+package OOP.Lesson.Les06;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Single responsibility principle	
+// Принцип единственной ответственности
+// У класса есть предназначение
+
+public class Ex001_3SRP {
+    public static void main(String[] args) {
+
+        PlannerSRP planner = new PlannerSRP();
+        planner.addEntry("1");
+        planner.removeEntry(0);
+        // planner.saveAllText("path");
+        // planner.loadFromDB("10.0.0.0","admin","1234");
+        DataManager dm = new DataManager();
+        dm.saveAllText(planner, "c:\\безысходность\\srp.txt");
+
+    }
+}
+
+// Планировщик с двумя функциями, удаление и добавление данных
+class PlannerSRP {
+    List<String> entries = new ArrayList<>();
+
+    public void addEntry(String text) {
+        entries.add(text);
+    }
+    public void removeEntry(int index) {
+        entries.remove(index);
+    }
+    @Override
+    public String toString() {
+        return String.join("\n", entries);
+    }    
+}
+
+
+class DataManager {
+    void save( PlannerSRP planner, String path);
+
+}
+
+class FileManager extends DataManager {
+    private boolean check(String filename){
+        return true;
+    }
+
+    public void saveAllText(PlannerSRP planner,String path) {
+        /// ошибки связанные с...
+        if(check(path));///
+    }
+
+    private void save( PlannerSRP planner, String path) {
+        this.saveAllText(planner, path);
+    };
+}
+
+class DbManeger extends DataManager {
+
+    private void saveDb(PlannerSRP planner, String url, String username, String password) {
+        /// ошибки связанные с ...
+        if (check(url)); ///
+        save(planner, new ContentProvider(url, username, password).getPath());
+    }
+}
+
+
+class ContentProvider {
+    String uri;
+    String username;
+    String password;
+    public ContentProvider(String uri, String username, String password) {
+        this.uri = uri;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getPath() {
+        return "...";
+    }
+}
