6ed9ce2f22a39643479a23d32ca2f75a57663f1b
Vit <mayllby@gmail.com>
2024-03-21 21:29:37 +0300

    Добавил код

--- Lesson/Les06/Ex000_Info.java
+++ Lesson/Les06/Ex000_Info.java
@@ -157,3 +157,35 @@ Open-closed principle
 Ex002_1OCP (см. код ниже)
 
 */
+
+// Open-closed principle
+// Принцип открытости/закрытости
+// Можно улучшать, но нельзя изменять
+
+/* public */ class Ex002_1OCP {
+    public static void main(String[] args) {
+        System.out.println(new Client(1).getDiscount(100));
+    }
+}
+
+class Client {
+    public int clientType;
+
+    public Client(int type) {
+        clientType = type;
+    }
+
+    /*
+     * Метод предоставления скидок клиентам
+     * в зависимости от их статуса
+     * Метод ограничен в расширении списка 
+     * предоставляемых скидок разным клиентам.
+     * В случае необходимости расширения списка, 
+     * код придётся переписать полностью.
+     */
+    public double getDiscount(double price) {
+        return (clientType == 1) ? price *0.85 : price *0.95;
+    }
+}
+
+// Больше клиентов = больше if'ов
