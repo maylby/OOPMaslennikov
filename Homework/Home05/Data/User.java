7fc8be91555e3afdd4cd13c1a2c302619758ac9e
Vit <mayllby@gmail.com>
2024-03-21 08:39:09 +0300

    Добавил код

---
+++ Homework/Home05/Data/User.java
@@ -0,0 +1,35 @@
+/*
+Объектно-ориентированное программирование (семинары)
+Урок 5. От простого к практике
+https://gb.ru/lessons/414500/homework
+
+ */
+package OOP.Homework.Home05.Data;
+
+/*
+* Класс "User"
+* получает и вносит в набор
+* имена (name) и возраст (age)
+*/
+public abstract class User {
+
+	protected String name;
+	protected int age;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
+
