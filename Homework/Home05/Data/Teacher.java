76f0b36e89c1514f0a9f2e222dbf88480b3c71ff
Vit <mayllby@gmail.com>
2024-03-21 08:33:35 +0300

    Добавил код

---
+++ Homework/Home05/Data/Teacher.java
@@ -0,0 +1,40 @@
+/*
+Объектно-ориентированное программирование (семинары)
+Урок 5. От простого к практике
+https://gb.ru/lessons/414500/homework
+
+ */
+package OOP.Homework.Home05.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+* Класс "Teacher" реализует (наследует) 
+* абстрактный класс "User", формирует
+* список (List) данных о преподавателе
+*/
+public class Teacher extends User {
+	private List<String> disciples;
+
+	public Teacher(String name, int age, List<String> disciples) {
+		this.name = name;
+		this.age = age;
+		this.disciples = disciples;
+
+	}
+
+	public List<String> getDisciples() {
+		return disciples;
+	}
+
+	public void setDisciples(ArrayList<String> disciples) {
+		disciples = new ArrayList<>();
+	}
+
+	@Override
+	public String toString() {
+	return "Student [name = " + this.name + 
+				"age = " + this.age + 
+				"id = " + this.disciples + "]";
+	}}
