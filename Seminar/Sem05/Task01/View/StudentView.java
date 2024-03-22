02436411affcf5e4df23e9ce363b4d5ff724636c
Vit <mayllby@gmail.com>
2024-03-22 13:08:09 +0300

    Добавил код

---
+++ Seminar/Sem05/Task01/View/StudentView.java
@@ -0,0 +1,25 @@
+/*
+Объектно-ориентированное программирование (семинары)
+Урок 5. От простого к практике
+https://gb.ru/lessons/414500
+
+ */
+package OOP.Seminar.Sem05.Task01.View;
+
+import java.util.ArrayList;
+
+import OOP.Homework.Home03.Student;
+
+public class StudentView {
+    public StudentView() {
+
+    }
+
+    public void studCheck(ArrayList<Student> students, int id) {
+        for (Student student : students) {
+            if (student.getId() == id) {
+                System.out.println(student);
+            }
+        }
+    }
+}
