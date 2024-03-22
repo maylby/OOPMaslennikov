44c483ddd960f88f5bded3f8ece7a0eb3d2da2da
Vit <mayllby@gmail.com>
2024-03-22 14:44:55 +0300

    Добавил 'Employe'

---
+++ Seminar/Sem06/Task01/Employe.java
@@ -0,0 +1,14 @@
+package OOP.Seminar.Sem06.Task01;
+
+public class Employe {
+    private String name;
+    private double salary;
+
+    public void calculateSalary() {
+        // подсчёт заработной платы
+    }
+    
+    public void saveEmploye() {
+        // сохранение данных о сотруднике в базу данных
+    }
+}
