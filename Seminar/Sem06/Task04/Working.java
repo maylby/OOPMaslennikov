07d0637022498f12aa0c2f10c94c82cb4269d947
Vit <mayllby@gmail.com>
2024-03-22 20:25:06 +0300

    Добавил заголовок

---
+++ Seminar/Sem06/Task04/Working.java
@@ -0,0 +1,24 @@
+/*
+Объектно-ориентированное программирование (семинары)
+Урок 6. ООП Дизайн и Solid
+https://gb.ru/lessons/414501
+
+
+ */
+package OOP.Seminar.Sem06.Task04;
+
+/**
+ * 
+ * Working
+ */
+interface Working {
+    void work();
+}
+
+/**
+ * Eating
+ */
+interface Eating {
+    void eat();    
+}
+
