38ae0f1d94c8e396f46c709e11b1cda0ac6d7217
Vit <mayllby@gmail.com>
2024-03-22 18:00:33 +0300

    Добавил заголовок

--- Seminar/Sem06/Task02/AreaCalculator.java
+++ Seminar/Sem06/Task02/AreaCalculator.java
@@ -1,3 +1,9 @@
+/*
+Объектно-ориентированное программирование (семинары)
+Урок 6. ООП Дизайн и Solid
+https://gb.ru/lessons/414501
+
+ */
 package OOP.Seminar.Sem06.Task02;
 
 public class AreaCalculator {
