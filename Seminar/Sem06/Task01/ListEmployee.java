b2f2cc436b688f76135002d3b5b8fbd182fa7959
Vit <mayllby@gmail.com>
2024-03-22 15:49:29 +0300

    Добавил пояснения

--- Seminar/Sem06/Task01/ListEmployee.java
+++ Seminar/Sem06/Task01/ListEmployee.java
@@ -3,8 +3,8 @@ package OOP.Seminar.Sem06.Task01;
 import java.util.ArrayList;
 
 public class ListEmployee {
-    ArrayList<Employee> EmployeeList = new ArrayList<Employee>();
-    
+    ArrayList<Employee> EmployeeList = new ArrayList<>(); // справа, в угловых скобках, 
+                                                          // "Employee" можно не писать
     public void saveEmployee(Employee employee) {
         EmployeeList.add(employee);
     }
