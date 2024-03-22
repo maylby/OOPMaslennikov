fd1786a9da549b2db2bacf67109783ff9d17a366
Vit <mayllby@gmail.com>
2024-03-22 21:27:38 +0300

    Добавил код

--- Seminar/Sem06/Task05/LightBulb.java
+++ Seminar/Sem06/Task05/LightBulb.java
@@ -5,10 +5,30 @@ https://gb.ru/lessons/414501
 
 01:33:00
 
+Принцип DIP
 Dependency Inversion Principle (Принцип инверсии зависимостей)
  */
 package OOP.Seminar.Sem06.Task05;
 
 public class LightBulb {
+    public void turnOn() {
+        // Включение лампочки
+    }
     
+    public void turnOff() {
+        // Выключение лампочки
+    }
+    }
+    
+class Switch {
+    private LightBulb bulb;
+    
+    public Switch() {
+        this.bulb = new LightBulb();
+    }
+    
+    public void operate() {
+        // Работа с выключателем
+        bulb.turnOn();
+    }
 }
