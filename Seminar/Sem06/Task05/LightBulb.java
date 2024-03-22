eb1df1dd43bfd44a6aa38b1d979184230d626941
Vit <mayllby@gmail.com>
2024-03-22 21:24:58 +0300

    Добавил заголовок

---
+++ Seminar/Sem06/Task05/LightBulb.java
@@ -0,0 +1,14 @@
+/*
+Объектно-ориентированное программирование (семинары)
+Урок 6. ООП Дизайн и Solid
+https://gb.ru/lessons/414501
+
+01:33:00
+
+Dependency Inversion Principle (Принцип инверсии зависимостей)
+ */
+package OOP.Seminar.Sem06.Task05;
+
+public class LightBulb {
+    
+}
