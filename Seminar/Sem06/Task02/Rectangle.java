c0cc4c91518b629dde9cc9b2ddb11ae4b3f1682f
Vit <mayllby@gmail.com>
2024-03-22 17:37:12 +0300

    Добавил код

---
+++ Seminar/Sem06/Task02/Rectangle.java
@@ -0,0 +1,12 @@
+package OOP.Seminar.Sem06.Task02;
+
+public class Rectangle {
+    public double width;
+	public double height;
+}
+
+class AreaCalculator {
+	public double calculateArea(Rectangle rectangle) {
+	    return rectangle.width * rectangle.height;
+	}
+}
