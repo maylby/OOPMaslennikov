94c37c57f05a12caec3b6f806b716eb92b68057c
Vit <mayllby@gmail.com>
2024-03-22 17:58:38 +0300

    Добавил код

---
+++ Seminar/Sem06/Task02/AreaCalculator.java
@@ -0,0 +1,8 @@
+package OOP.Seminar.Sem06.Task02;
+
+public class AreaCalculator {
+
+	public double calculateArea(Rectangle rectangle) {
+        return rectangle.width * rectangle.height;
+    }
+}
