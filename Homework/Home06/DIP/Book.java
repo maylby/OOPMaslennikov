60e93c265c022613dc8a2ad37632a419e0face25
Vit <mayllby@gmail.com>
2024-03-24 20:11:08 +0300

    Добавил 'Book'

---
+++ Homework/Home06/DIP/Book.java
@@ -0,0 +1,39 @@
+/*
+Объектно-ориентированное программирование (семинары)
+Урок 4. ООП: Обобщения. ч1
+https://gb.ru/lessons/414499/homework
+
+ */
+package OOP.Homework.Home04;
+
+/*
+* Book - книгa в библиотеке
+* параметр типа T - ее название
+*/
+public class Book<T> {
+    private T name;
+    private String autor;
+    private Integer pages;
+    
+    public Book(T name, String autor, Integer pages) {
+        this.name = name;
+        this.autor = autor;
+    }
+    
+    public T getName() {
+        return name;
+    }
+    
+    public String getAutor() {
+        return autor;
+    }
+
+    public Integer getPages() {
+        return pages;
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("Книга %s", name, pages);
+    }
+}
