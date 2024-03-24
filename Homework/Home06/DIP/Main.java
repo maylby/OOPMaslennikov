886de14b626c0e51ec90b174d950dd8d0c55ad58
Vit <mayllby@gmail.com>
2024-03-24 20:22:03 +0300

    Добавил 'Main'

---
+++ Homework/Home06/DIP/Main.java
@@ -0,0 +1,41 @@
+/*
+Объектно-ориентированное программирование (семинары)
+Урок 6. ООП Дизайн и Solid
+https://gb.ru/lessons/414501/homework
+
+
+Реализация DIP 
+Принцип инверсии зависимостей
+*/
+package OOP.Homework.Home06.DIP;
+
+/*
+Создайте обобщенный класс Book<T>, который представляет книгу в библиотеке. 
+Каждая книга должна содержать параметр типа T для ее названия.
+Создайте обобщенный класс LibraryCard<K, V>, который представляет
+библиотечную карточку для определенной книги.
+ 
+Карточка должна содержать параметры типов K (номер карточки) и V (информация о книге).
+Создайте класс Library, который будет представлять библиотеку. 
+У этого класса должны быть методы добавления книг и выдачи библиотечных карточек.
+Добавьте метод для вывода информации о всех выданных книгах с их библиотечными карточками.
+*/
+public class Main {
+    public static void main(String[] args) {
+
+    	Library<String, Integer, String> library = new Library<>(); 
+        
+    	library.addBook(new Book<>("book", "LastName", 1991)); 
+    	library.addBook(new Book<>("jornal", "Edition", 2011));
+
+    	LibraryCard<Integer, String> card1 = new LibraryCard<Integer, String>(0021); 
+   	    LibraryCard<Integer, String> card2 = new LibraryCard<Integer, String>(0012);
+    
+    	card1.setListBook("book");
+    	card2.setListBook("jornal");
+    	library.createLibraryCard(card1);
+    	library.createLibraryCard(card2);
+
+    	System.out.println(library);
+    }
+}
