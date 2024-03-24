5fc1254625013d309cafb5e0f6c71ec9deb342e2
Vit <mayllby@gmail.com>
2024-03-24 20:46:34 +0300

    Добавил 'LibraryCard'

---
+++ Homework/Home06/OCP/LibraryCard.java
@@ -0,0 +1,40 @@
+/*
+Объектно-ориентированное программирование (семинары)
+Урок 6. ООП Дизайн и Solid
+https://gb.ru/lessons/414501/homework
+
+
+Реализация LSP 
+Принцип подстановки Барбары Лисков
+*/
+package OOP.Homework.Home06.LSP;
+
+import java.util.ArrayList;
+
+/*
+* "LibraryCard" - библиотечная карточка определенной книги
+* "K" - номер карточки, "V" - информация о книге
+*/
+public class LibraryCard<K, V> {
+    private K number;
+    private ArrayList<V> listBook = new ArrayList<>();
+
+    public LibraryCard(K number) {
+        this.number = number;
+    }
+
+    public ArrayList<V> getListBook() {
+        return listBook;
+    }
+
+    public void setListBook(V book) {
+        listBook.add(book);
+    }
+
+    @Override
+    public String toString() {
+        return  "Книга:{ " + number +
+                ", Аннотация: " + listBook +
+                '}';
+    }
+}
