376631f49aa0034ab8bf320f839b2f3b9e31318c
Vit <mayllby@gmail.com>
2024-03-24 19:13:47 +0300

    Изменил код

---
+++ Homework/Home06/SRP/Library.java
@@ -0,0 +1,43 @@
+package OOP.Homework.Home06.SRP;
+
+/*
+Объектно-ориентированное программирование (семинары)
+Урок 6. ООП Дизайн и Solid
+https://gb.ru/lessons/414501/homework
+*/
+
+
+/*
+* "Library" - библиотека
+* метод добавления книг 
+* метод выдачи библиотечных карточек
+* метод вывода информации о всех выданных книгах 
+* с их библиотечными карточками.
+*/
+public class Library<T, K, V> {
+
+    private List<Book<T>> books;
+    private int cardCount;
+    private List<LibraryCard<K, V>> orders; // Order
+
+    public OnlineStore() {
+        books = new ArrayList<>();
+        libraryCard = new ArrayList<>();
+        cardCount = 0;
+    }
+
+    void addBook(Book<T> book) {
+        books.add(book);
+    }
+
+    void createLibraryCard(LibraryCard<K, V> libraryCard) {
+        libraryCards.add(libraryCard);
+        cardCount++;
+    }
+
+    @Override
+    public String toString() {
+        return "list = " + "№" + cardCount + libraryCards;
+    }
+
+}
