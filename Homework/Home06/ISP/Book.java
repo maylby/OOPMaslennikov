efd7d1eb97690df5b1e4ee3aab94b22d6aaa59a9
Vit <mayllby@gmail.com>
2024-03-24 20:31:57 +0300

    Добавил 'Book'

---
+++ Homework/Home06/ISP/Book.java
@@ -0,0 +1,42 @@
+/*
+Объектно-ориентированное программирование (семинары)
+Урок 6. ООП Дизайн и Solid
+https://gb.ru/lessons/414501/homework
+
+
+Реализация DIP 
+Принцип инверсии зависимостей
+*/
+package OOP.Homework.Home06.DIP;
+
+/*
+* Book - книгa в библиотеке
+* параметр типа T - ее название
+*/
+public class Book<T> {
+    private T name;
+    private String autor;
+    private Integer pages;
+    
+    public Book(T name, String autor, Integer pages) {
+        this.name = name;
+        this.autor = autor;
+    }
+    
+    public T getName() {
+        return name;
+    }
+    
+    public String getAutor() {
+        return autor;
+    }
+
+    public Integer getPages() {
+        return pages;
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("Книга %s", name, pages);
+    }
+}
