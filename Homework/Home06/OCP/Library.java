7dda7e65d6dd825bc1205d5f8821e76f7482cc21
Vit <mayllby@gmail.com>
2024-03-24 20:46:50 +0300

    Добавил 'Library'

---
+++ Homework/Home06/OCP/Library.java
@@ -0,0 +1,48 @@
+/*
+Объектно-ориентированное программирование (семинары)
+Урок 6. ООП Дизайн и Solid
+https://gb.ru/lessons/414501/homework
+
+
+Реализация LSP 
+Принцип подстановки Барбары Лисков
+*/
+package OOP.Homework.Home06.LSP;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+* "Library" - библиотека
+* метод добавления книг 
+* метод выдачи библиотечных карточек
+* метод вывода информации о всех выданных книгах 
+* с их библиотечными карточками.
+*/
+public class Library<T, K, V> {
+
+    private List<Book<T>> books;
+    private int cardCount;
+    private List<LibraryCard<K, V>> libraryCards; 
+    
+    public Library() {
+        books = new ArrayList<>();
+        libraryCards = new ArrayList<>();
+        cardCount = 0;
+    }
+
+    void addBook(Book<T> book) {
+        books.add(book);
+    }
+
+    void createLibraryCard(LibraryCard<K, V> libraryCard) {
+        libraryCards.add(libraryCard);
+        cardCount++;
+    }
+
+    @Override
+    public String toString() {
+        return "list = " + "№" + cardCount + libraryCards;
+    }
+
+}
