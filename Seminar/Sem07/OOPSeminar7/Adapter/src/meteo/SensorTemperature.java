4bf54b3856131ab0da866cd4348d1b4fa8b5c80a
Vit <mayllby@gmail.com>
2024-03-25 21:28:55 +0300

    Добавил код

---
+++ Seminar/Sem07/OOPSeminar7/Adapter/src/meteo/SensorTemperature.java
@@ -0,0 +1,9 @@
+package OOP.Seminar.Sem07.OOPSeminar7.Adapter.src.meteo;
+
+interface SensorTemperature{
+	int identifier(); // идентификатор датчика
+	double temperature(); // температура датчика
+	int year();			 // Год
+	int day();			 // День года
+	int second();		 // Секунда дня
+}
