7e3b97fb3e67c68a8d14097e8b8e6a918f8e82c9
Vit <mayllby@gmail.com>
2024-03-25 22:34:16 +0300

    Добавил код

---
+++ Seminar/Sem07/OOPSeminar7/DecoratorFactory/out/production/Decorator/calculator/Calculable.java
@@ -0,0 +1,10 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+package OOP.Seminar.Sem07.OOPSeminar7.DecoratorFactory.out.production.Decorator.calculator;
+
+public interface Calculable {
+    Calculable sum(int var1);
+ 
+    Calculable multi(int var1);
+ 
+    int getResult();
+ }
