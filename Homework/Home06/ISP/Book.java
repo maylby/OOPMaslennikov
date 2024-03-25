7e1bf6cf7e8255e644d743546a1966e963a159ec
Vit <mayllby@gmail.com>
2024-03-25 11:22:02 +0300

    Добавил код

--- Homework/Home06/ISP/Book.java
+++ Homework/Home06/ISP/Book.java
@@ -6,8 +6,28 @@ https://gb.ru/lessons/414501/homework
 
 Реализация ISP 
 Принцип разделения интерфейса
+
+Добавлен интерфейс "Article" (статья)
+для поиска по текста статьи 
+(подстановка в периодические издания)
+Добавлен класс "Journal" (журнал)
+Добавлен класс "Newspaper" (газета)
 */
 package OOP.Homework.Home06.ISP;
+
+/**
+ * ArticleJournal
+ */
+interface ArticleJournal {
+    void text();
+}
+
+/**
+ * ArticleJournal
+ */
+interface ArticleNewspaper {
+    void text();
+}
 
 /*
 * Book - книгa в библиотеке
@@ -38,5 +58,43 @@ public class Book<T> {
     @Override
     public String toString() {
         return String.format("Книга %s", name, pages);
+    }
+}
+
+/*
+ * класс "Newspaper" (газета)
+ */
+class Newspaper<T> extends Book<T> implements ArticleNewspaper {
+
+    public Newspaper(T name, String autor, Integer pages) {
+        super(name, autor, pages);
+        
+    }
+
+    @Override
+    public void text() {
+        // метод поиска статьи в газете
+        // отличается от поиска в журнале
+        // нет оглавления (содержания)
+    }
+}
+
+
+/*
+ * класс "Journal" (журнал)
+ * наследует класс
+ */
+class Journal<T> extends Book<T> implements ArticleJournal {
+
+    public Journal(T name, String autor, Integer pages) {
+        super(name, autor, pages);
+        
+    }
+
+    @Override
+    public void text() {
+        // метод поиска статьи в журнале
+        // отличается от поиска в газете
+        // есть оглавление (содержания)
     }
 }
