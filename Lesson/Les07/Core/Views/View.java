183e99ba3bee41198f49ae16c46263638ad55576
Vit <mayllby@gmail.com>
2024-03-25 18:58:33 +0300

    Добавил 'View'

---
+++ Lesson/Les07/Core/Views/View.java
@@ -0,0 +1,12 @@
+/*
+Объектно-ориентированное программирование (лекции)
+Урок 5. ООП: От простого к практике
+https://gb.ru/lessons/414493
+
+ */
+package OOP.Lesson.Les05.Ex003.Core.Views;
+
+public interface View {
+    String get();
+    void set(String value);
+}
