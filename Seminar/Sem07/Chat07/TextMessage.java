a5d9949cd7ad62a37d1e751fb21a871c1b95fdc1
Vit <mayllby@gmail.com>
2024-03-27 16:35:38 +0300

    Добавил заголовок

--- Seminar/Sem07/Chat07/TextMessage.java
+++ Seminar/Sem07/Chat07/TextMessage.java
@@ -1,3 +1,9 @@
+/*
+Объектно-ориентированное программирование (семинары)
+Урок 7. ООП Дизайн и Solid ч.2
+https://gb.ru/lessons/414502
+
+ */
 package OOP.Seminar.Sem07.Chat07;
 
 public class TextMessage implements Message {
