df3d46f1a526ee03f3f3f966671d6ff46fd68050
Vit <mayllby@gmail.com>
2024-03-28 10:36:21 +0300

    Добавил заголовок

--- Homework/Home07/calculator/ICreateCalculation.java
+++ Homework/Home07/calculator/ICreateCalculation.java
@@ -1,5 +1,13 @@
+/*
+Объектно-ориентированное программирование (семинары)
+Урок 7. ООП Дизайн и Solid ч.2
+https://gb.ru/lessons/414502/homework
+
+Аттестационная работа
+
+ */
 package OOP.Homework.Home07.calculator;
 
 interface ICreateCalculation {
-    Calculable create(int primaryArg);
+    Calculable create(int firstArg);
 }
--- Homework/Home07/calculator/Main.java
+++ Homework/Home07/calculator/Main.java
@@ -1,5 +1,19 @@
+/*
+Объектно-ориентированное программирование (семинары)
+Урок 7. ООП Дизайн и Solid ч.2
+https://gb.ru/lessons/414502/homework
+
+Аттестационная работа
+
+ */
 package OOP.Homework.Home07.calculator;
 
+/*
+ * Создать проект калькулятора комплексных чисел 
+ * (сложение, умножение и деление). 
+ * Применить при создании программы 
+ * архитектурные паттерны, добавить логирование калькулятора.
+ */
 public class Main {
     public static void main(String[] args) {
         ICreateCalculation createCalculation = new CreateCalculation();
