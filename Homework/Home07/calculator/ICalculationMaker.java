8e3d096f9dc5ae0da4581941d25df6f85d664fdb
Vit <mayllby@gmail.com>
2024-03-28 13:25:51 +0300

    Добавил 'ICalculationMaker'

---
+++ Homework/Home07/calculator/ICalculationMaker.java
@@ -0,0 +1,13 @@
+/*
+Объектно-ориентированное программирование (семинары)
+Урок 7. ООП Дизайн и Solid ч.2
+https://gb.ru/lessons/414502/homework
+
+Аттестационная работа
+
+ */
+package OOP.Homework.Home07.calculator;
+
+interface ICalculationMaker {
+    Calculable create(int firstArg);
+}
